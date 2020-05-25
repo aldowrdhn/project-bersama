c9452988c78c4fc71eb2b1df5305c6abd7aec0fc
aldo1567 <ronaldo.aldo97@gmail.com>
2020-05-25 12:30:33 +0700

    Section Product Done

--- resources/views/Page/product.blade.php
+++ resources/views/Page/product.blade.php
@@ -18,4 +18,38 @@
         </div>
     </div>
 </section>
+<section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>Made By Fidi IT Kreatif</h1>
+            </div>
+        </div>
+        <div class="row my-5">
+            <div class="col-md-12 text-center">
+                <button type="button" class="btn btn-outline-warning d-inline-block mx-3" onclick="allproject()">All Project</button>
+                <button type="button" class="btn btn-outline-warning d-inline-block mx-3" onclick="ver1()">Versi 1</button>
+                <button type="button" class="btn btn-outline-warning d-inline-block mx-3" onclick="ver2()">Versi 2</button>
+                <button type="button" class="btn btn-outline-warning d-inline-block mx-3" onclick="ver3()">Versi 3</button>
+            </div>
+        </div>
+        <div class="row my-5">
+            <div class="col-md-4" id="ver1">
+                <div class="versi-box">
+                    <img src="http://fidiitkreatif.co.id/asset/produk/3.png" alt="" class="phone">
+                </div>
+            </div>
+            <div class="col-md-4" id="ver2">
+                <div class="versi-box">
+                    <img src="http://fidiitkreatif.co.id/asset/produk/2.png" alt="" class="phone">
+                </div>
+            </div>
+            <div class="col-md-4" id="ver3">
+                <div class="versi-box">
+                    <img src="http://fidiitkreatif.co.id/asset/produk/1.png" alt="" class="phone">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
