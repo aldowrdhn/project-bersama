c8981ca7e2134e827f50a7f1dadcc7fe11999f6b
aldo1567 <ronaldo.aldo97@gmail.com>
2020-05-26 21:29:26 +0700

    Pisahin Parallax dan Header

--- resources/views/Layout/include/header.blade.php
+++ resources/views/Layout/include/header.blade.php
@@ -11,39 +11,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     {{-- Ronaldo Css --}}
   <link rel="stylesheet" href="{{asset('CSS/ronaldo/product.css')}}">
-  <style>
-    .section{
-      background-color: #8AA5FC;
-      position: relative;
-    }
-    .section-text{
-      width: 100%;
-      height: 100%;
-      opacity: 100%;
-      font-size: 80px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      position: absolute;
-      color: white;
-      font-weight: bolder;
-    }
-    .section-text p{
-    position: relative;
-    z-index: 2;
-    }
-  .parallax{
-    background-image:url("http://fidiitkreatif.co.id/template/img/bg-img/8.jpg");
-    /* Set a specific height */
-    min-height: 500px; 
-    opacity: 40%;
-    /* Create the parallax scrolling effect */
-    background-attachment: fixed;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-}
-  </style>
+  
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-3" style="background-color: #8AA5FC;">
@@ -68,12 +36,4 @@
       </div>
     </div>
   </nav>
-  <section>
-    <div class="section">      
-      <div class="section-text">
-      <p>@yield('section-head')</p>
-      </div>
-        <div class="parallax"></div>
-  </div>
   
-  </section>
