153fce5f9307d12feb0cf6e2bcb1560142a765ab
aldo1567 <ronaldo.aldo97@gmail.com>
2020-05-26 21:05:22 +0700

    Add Paralax Effect

--- resources/views/Layout/include/header.blade.php
+++ resources/views/Layout/include/header.blade.php
@@ -11,9 +11,42 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     {{-- Ronaldo Css --}}
   <link rel="stylesheet" href="{{asset('CSS/ronaldo/product.css')}}">
+  <style>
+    .section{
+      background-color: #8AA5FC;
+      position: relative;
+    }
+    .section-text{
+      width: 100%;
+      height: 100%;
+      opacity: 100%;
+      font-size: 80px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      color: white;
+      font-weight: bolder;
+    }
+    .section-text p{
+    position: relative;
+    z-index: 2;
+    }
+  .parallax{
+    background-image:url("http://fidiitkreatif.co.id/template/img/bg-img/8.jpg");
+    /* Set a specific height */
+    min-height: 500px; 
+    opacity: 40%;
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+  </style>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-dark py-3" style="background-color: #8AA5FC;">
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-3" style="background-color: #8AA5FC;">
     <div class="row">
       <div class="col-sm-12 col-md-6">
         <a class="navbar-brand" href="/"><i class="fas fa-percentage"></i></a>
@@ -35,3 +68,12 @@
       </div>
     </div>
   </nav>
+  <section>
+    <div class="section">      
+      <div class="section-text">
+      <p>@yield('section-head')</p>
+      </div>
+        <div class="parallax"></div>
+  </div>
+  
+  </section>
